aa2cb0d6003d6c2dab0d672afa965c2fc4c00e7b
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-11-23 22:32:50 -0600

    refactor: ApiRepository

--- app/src/main/java/com/erickpimentel/mercadoeditorial/repository/ApiRepository.kt
+++ app/src/main/java/com/erickpimentel/mercadoeditorial/repository/ApiRepository.kt
@@ -4,8 +4,6 @@ import com.erickpimentel.mercadoeditorial.api.ApiServices
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
-class ApiRepository @Inject constructor(
-    private val apiServices: ApiServices
-) {
+class ApiRepository @Inject constructor(private val apiServices: ApiServices) {
     suspend fun getBooks(page: Int?, type : String?, statusCode: Int?, title: String?, isbn: String?, price: String?, synopsis: String?) = apiServices.getBooks(page, type, statusCode, title, isbn, price, synopsis)
 }
